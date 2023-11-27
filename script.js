b4e139b057223a237ec33b62322d80ea9f733525
Aai'sha Fisher <aaishafisher484@gmail.com>
2023-11-27 12:17:57 +0200

    Fix previous incorrect code

---
+++ script.js
@@ -0,0 +1,35 @@
+const order1 = document.querySelector('section');
+const biscuitsOrder1=  order1.querySelector('.biscuits .count');
+const donutsOrder1 = order1.querySelector('.donuts .count');
+const pancakeOrder1 = order1.querySelector('.pancakes .count');
+const statusOrder1 = order1.querySelector('.status dd')
+
+1-biscuits= 1-root.biscuits,
+1-donuts = 1-root.donuts,
+1-pancakes = 1-root.pancakes,
+1-status = 1-root.status ? Delivered : Pending
+
+
+const 2-root = document(order2),
+const 2-biscuits: document(biscuits),
+const 2-donuts: document(donuts),
+const 2-pancakes: document(pancakes),
+const 2-status: document(status)
+
+const 3-root = document(order3),
+const 3-biscuits: document(biscuits),
+const 3-donuts: document(donuts),
+const 3-pancakes: document(pancakes),
+const 3-status: document(status)
+
+
+
+2-biscuits= 2-root.biscuits,
+2-donuts = 2-root.donuts,
+2-pancakes = 2-root.pancakes,
+2-status = 2-root.status ? Delivered : Pending
+
+3-biscuits= 3-root.biscuits,
+3-donuts = 3-root.donuts,
+3-pancakes = 3-root.pancakes,
+3-status = 3-root.status ? Delivered : Pending
